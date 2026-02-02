1fe2efe4a191472c4cb66f7e34ad78a8275bcf92
dongki07 <01053928269k@gmail.com>
2026-02-02 23:21:30 +0900

    Setting controller

---
+++ src/main/java/com/example/shoppingMall/controller/FirstController.java
@@ -0,0 +1,14 @@
+package com.example.shoppingMall.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/main")
+public class FirstController {
+    @GetMapping("/test")
+    public void test() {
+        System.out.println("Testing");
+    }
+}
